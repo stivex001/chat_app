80b4990d2acd8f1856c4226d374d8c466b532cbd
stivex001 <stephenadeyemo@gmail.com>
2025-08-31 13:52:10 +0100

    feat:added new variables to the chat list

--- src/components/chats/ChatCenter.tsx
+++ src/components/chats/ChatCenter.tsx
@@ -1,6 +1,8 @@
-"use client"
-import React, { useEffect, useRef } from 'react';
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
 import { Messages } from './Messages';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '@/lib/firebase';
 
 const mockMessages = [
   {
@@ -27,11 +29,22 @@ const mockMessages = [
 export const ChatCenter = () => {
   const currentUserId = 'user1';
   const endRef = useRef<HTMLDivElement>(null);
+  const [chats, setChats] = useState<any[]>([]);
 
   useEffect(() => {
     endRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [])
-  
+  }, []);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, 'chats', 'NWLE20U1iicAxQ2Nrg9q'), res => {
+      const data = res?.data();
+      setChats(Array.isArray(data?.messages) ? data.messages : []);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, []);
 
   return (
     <div className="no-scrollbar flex flex-1 flex-col gap-5 overflow-y-scroll p-5">
